56a2cec7fcea9c6d0b43e074e78c5a54ca6ed82a
Leo Saporito <leosaporito16@gmail.com>
2026-01-13 16:32:42 -0500

    Did this commit work?

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Did this work?");
+
+        Debug.Log("Did this work now?");
+
+        Debug.Log("How about now?");
+
+        // test commit from Leo
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
