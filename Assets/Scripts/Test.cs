19cbf2e6bc0ec9427fa63a9ea1d4a60808122aa2
Leo Saporito <leosaporito16@gmail.com>
2026-01-14 17:50:39 -0500

    Added in the Follow Mouse and Mover scripts

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,26 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class FirstScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 0.01f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Did this work?");
-
-        Debug.Log("Did this work now?");
-
-        Debug.Log("How about now?");
-
-        // test commit from Leo
-
-        //can I change now
+        //speed = Random.Range(0.01f, 0.3f);
+        transform.position = (Vector2)transform.position + Random.insideUnitCircle * 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Moves the square
+        Vector3 newPosition = transform.position;
+        newPosition.x += speed;
+        transform.position = newPosition;
+
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        if (screenPos.x < 0 || screenPos.x > Screen.width)
+        {
+            speed *= -1;
+        }
     }
 }
