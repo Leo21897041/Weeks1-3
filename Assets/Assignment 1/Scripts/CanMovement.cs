768f00918eb2f4a6585072e2e6e4029fff5483cc
Leo Saporito <leosaporito16@gmail.com>
2026-01-28 00:22:57 -0500

    Added comments to the Crosshair and CanMovement scripts

--- Assets/Assignment 1/Scripts/CanMovement.cs
+++ Assets/Assignment 1/Scripts/CanMovement.cs
@@ -3,58 +3,75 @@ using UnityEngine.InputSystem;
 
 public class CanMovement : MonoBehaviour
 {
+    //Variables to store the position or speed of the can
     public Vector3 position;
     public float speed;
     public float maxHeight;
     public float minHeight;
 
+    //Vector variables to store the position of the mouse on the screen, then convert it to the location in the world space
     public Vector3 mousePosition;
     public Vector3 mouseWorldPosition;
 
+    //Booleans to switch the direction of the can
     public bool isUp = false;
     public bool isDown = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        isUp = true;   
+        //Start the program with the can moving up
+        isUp = true;
+        isDown = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //adding movement to each boolean when it is active
         if (isUp == true)
         { 
             position.y += Time.deltaTime * speed;
         }
-
         if (isDown == true)
         {
             position.y -= Time.deltaTime * speed;
         }
 
+        //Finds the position of the can in the world space and converts it to the position on the screen space
         Vector3 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
-
-        minHeight = 0f;
+        //Sets the min and max height the cans can go before switching directions  
         maxHeight = Screen.height - 50;
+        minHeight = 0f;
 
+        //Switches the booleans which switches the direction when the can reaches a certain position on the screen
+        //Switches direction on the top
         if (screenPosition.y > maxHeight)
         {
             isUp = false;
             isDown = true;
         }
+
+        //Switches direction on the bottom
         if (screenPosition.y < minHeight)
         {
             isUp = true;
             isDown = false;
         }
 
-            transform.position = position;
+        //sets the position under the transform component to the position variable
+        transform.position = position;
 
+        //Finds the mouse position on the screen, then finds and converts the position on the screen to the location on the world space
         mousePosition = Mouse.current.position.ReadValue();
         mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        //sets the mouse position on the z axis to 0 to make sure its aligned with all other objects
         mouseWorldPosition.z = 0f;
+
+        //finds the distance between the mouse and the can
         float distanceToMouse = Vector3.Distance(mouseWorldPosition, transform.position);
 
+        //switches the direction upwards but only if the can only if it is moving downwards
         if (distanceToMouse < 1)
         {
             isUp = true;
--- Assets/Assignment 1/Scripts/Crosshair.cs
+++ Assets/Assignment 1/Scripts/Crosshair.cs
@@ -12,7 +12,9 @@ public class Crosshair : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //finds the mouse position on the screen and convert it to the game objects position in the world space
         Vector2 currentMousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        //sets the position of the game object in the world space to the position of the mouse in the screen space
         transform.position = currentMousePosition;
     }
 }
