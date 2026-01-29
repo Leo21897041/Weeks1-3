7f3852787682b1c81e68372c0f2e192b83b01faa
Leo Saporito <leosaporito16@gmail.com>
2026-01-29 03:49:24 -0500

    Added comments annotating the HideBird and the DogUpAndDown script.

--- Assets/Assignment 1/Scripts/DogUpAndDown.cs
+++ Assets/Assignment 1/Scripts/DogUpAndDown.cs
@@ -18,9 +18,11 @@ public class DogUpAndDown : MonoBehaviour
     public bool isGoingUp = false;
     public bool isGoingDown = false;
     public bool isAtBottom = false;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    //Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+    //Starts the game with the dog moving up
         isGoingUp = true;
         isGoingDown = false;
     }
@@ -28,50 +30,62 @@ public class DogUpAndDown : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    //Adds movement and a timer to the boolean to move the dog up
         if (isGoingUp == true)
         {
+            //moves the dog up if the dog is less than the max height it can go
             if (dogPosition.y < maxHeight)
             {
                 dogPosition.y += Time.deltaTime * speed;
             }
+            //starts a timer for when the dog does reach the top
             if (dogPosition.y > maxHeight)
             {
                 timer += Time.deltaTime * timeSpeed;
             }
         }
-        
+    //When the timer exceeds the maximumTimer value, switch directions of the dog
         if (timer > maxTimer)
         {
             isGoingUp = false;
             isGoingDown = true;
         }
 
+    //Adds movement and a timer to the boolean to move the dog down
         if (isGoingDown == true)
         {
+            //Move the dog down if the dog's y position is greater than the minimumHeight value
             if (dogPosition.y > minHeight)
             { 
                 dogPosition.y -= Time.deltaTime * speed;
             }
+            //When the dog's position passes the minnimum height, start the timer 
             if (dogPosition.y < minHeight)
             {
                 timer -= Time.deltaTime * timeSpeed;
             }
         }
-
+    //Moves the dogs position to a random position on the x axis before moving back up
         if (timer < minTimer)
         {
+            //sets the timer to back to the same value
             timer = minTimer;
+            //Creates a Vector3 variable for the dogs position
             Vector3 screenPosition = dogPosition;
+            //Moves the dog to a random position on the x axis
             screenPosition.x = Random.Range(0, Screen.width);
+
+            //Takes the variables screen position value, then converts and sets it to the world position
             dogPosition = Camera.main.ScreenToWorldPoint(screenPosition);
             dogPosition.y = minHeight;
             dogPosition.z = 0;
 
-            //dogPosition = screenPosition;
+    //Move the dog up
             isGoingUp = true;
             isGoingDown = false;
         }
 
+    //Sets this conversion to the values in the inspector
         transform.position = dogPosition;
     }
 }
--- Assets/Assignment 1/Scripts/HideBird.cs
+++ Assets/Assignment 1/Scripts/HideBird.cs
@@ -3,9 +3,11 @@ using UnityEngine.InputSystem;
 
 public class HideBird : MonoBehaviour
 {
-    public Vector3 hidePosition;
-    public bool isWorking = false;
+    //variables for position and lowest height it can spawn
+    public Vector3 newPosition;
     public float minHideHeight = -3.5f;
+    //debug
+    public bool isWorking = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,24 +18,40 @@ public class HideBird : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Variable for the current position of the mouse on the screen space
         Vector3 mousePosition = Mouse.current.position.ReadValue();
+        //Variable for converting the value position of the mouse from the screen space to finding the position of the mouse in the world space
         Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        //Sets the z position of the mouse to 0. Stays in line with all the other game assets
         mouseWorldPosition.z = 0f;
+
+        //finds the distance from the mouse to the bird
         float mouseToObject = Vector3.Distance(mouseWorldPosition, transform.position);
 
+        //runs this if the mouse is less than one unit from the object
         if (mouseToObject < 1)
         {
-            Vector3 screenPosition = hidePosition;
+            //Variable for setting the position of the bird to the new position
+            Vector3 screenPosition = newPosition;
+            //Move the bird to a random position on the x axis
             screenPosition.x = Random.Range(0, Screen.width);
+            //Always move the bird to this height on the y axis
             screenPosition.y = minHideHeight;
-            hidePosition = screenPosition;
-            hidePosition = Camera.main.ScreenToWorldPoint(screenPosition);
-            hidePosition.z = 0;
+
+            //Set the newPosition Vector3 variable to the random x and consistent y values
+            newPosition = screenPosition;
+            //Convert the position of the birds in the world space to the position of the birds in the screen space
+            newPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+            newPosition.z = 0;
+            //Sets this conversion to the values in the inspector
+            transform.position = newPosition;
+
+            //This is just a debug to see if this if statement was working
             isWorking = true;
-            transform.position = hidePosition;
         }
         else 
         {
+            //This is just a debug to see if this if statement was working
             isWorking = false;
         }
     }
