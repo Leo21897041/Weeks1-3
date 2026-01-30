b796395c0aa55f24ca9b0005e3f3834ad61968d4
Leo Saporito <leosaporito16@gmail.com>
2026-01-29 21:40:10 -0500

    Quick commit before I pull.

--- Assets/Scripts/In-Class Scripts/Looker.cs
+++ Assets/Scripts/In-Class Scripts/Looker.cs
@@ -12,7 +12,6 @@ public class Looker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.position moves object
         Vector3 currentRotation = transform.eulerAngles;
         currentRotation.z += Time.deltaTime * rotationSpeed;
         transform.eulerAngles = currentRotation;
